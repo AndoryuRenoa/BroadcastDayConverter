094437f433a9c190b2619995409b2b09ad5cd150
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-29 16:59:12 -0600

    Update bcdConverter.js

--- bcdConverter.js
+++ bcdConverter.js
@@ -10,6 +10,7 @@ var rotation;
 var tMethod;
 var updatedRaw;
 var findNext;
+var findLast;
 
 function runRawData(){
   //divide and place the corresponding raw data into the correct divided data below
@@ -23,15 +24,20 @@ function runRawData(){
   //sTime is next, however it could be either XX:XX or X:XX so you have to take that into account!
   // I'm going to use spaces to cut apart the different parts moving forward, I'll need a new variable i.e. updatedRaw
   var updatedRaw = rawData.slice(18);
-  var findNext = updatedRaw.search(/\s/);  
-  var sTime = updatedRaw.slice(0,findNext+3); // this still needs to be worked with
-  sTime.trim();// <-- this line causes problems
-  document.getElementById("startTime").value=sTime;
-  var updatedRaw = updatedRaw.slice(findNext+3); // same here
   var findNext = updatedRaw.search(/\s/);
+  var sTime = updatedRaw.slice(0,findNext+3); // this still needs to be worked with
+  sTime.trim(); // need to do sTime = sTime.trim(); for this to have effect
+  document.getElementById("startTime").value=sTime;
+  var updatedRaw = updatedRaw.slice(findNext+4); // same here
+  var findNext = updatedRaw.search(" ");
   var eTime = updatedRaw.slice(0,findNext+3); // same here
   eTime.trim();
   document.getElementById("endTime").value=eTime;
+  var updatedRaw = updatedRaw.slice(findNext); // below this line doesn't work
+  var findLast = updatedRaw.lastIndexOf(" ");
+  var tMethod = updatedRaw.splice(findLast, updatedRaw.length);
+  tMethod.trim();
+  document.getElementById("transferMethod").value=tMethod;
 }
 
 function runDivData(){
