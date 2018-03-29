6e916c6e31e1417de325f4b98f333ad9e0c90961
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-29 08:09:23 -0600

    Update bcdConverter.js

--- bcdConverter.js
+++ bcdConverter.js
@@ -1,1 +1,13 @@
+// variables go here
 
+function runRawData(){
+  //divide and place the corresponding raw data into the correct divided data below
+  alert("Still a WIP")
+  //runDivData();
+}
+
+function runDivData(){
+  // this will house the logic for converting from regular day into broadcast day
+  alert("Still a WIP")
+  //output will go in elementById"output"
+}
